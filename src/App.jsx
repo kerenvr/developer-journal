f983679b7721c9d9da7d6bd5909a2c770dcfaf4a
Keren Villatoro <kerenv@outlook.com>
2024-10-27 18:59:29 -0700

    react router retup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import Navbar from "./components/navbar/Navbar"
+import AboutPage from "./pages/AboutPage"
+import BlogPage from "./pages/BlogPage"
+import HomePage from "./pages/HomePage"
 import JournalPage from "./pages/JournalPage"
+import NewsletterPage from "./pages/NewsletterPage"
+import { Route,Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <>
-      <JournalPage />
-    </>
+    <div className="mx-auto">
+      <Navbar />
+      <div>
+        <div className="p-10">
+        <JournalPage />
+          <Routes>        
+            <Route path="/"  element={<HomePage/>} />
+            <Route path="/about"  element={<AboutPage />}/>
+            <Route path="/blog"  element={<BlogPage/>}/>
+            <Route path="/newsletter"  element={<NewsletterPage/>}/>
+          </Routes>
+        </div>
+      </div>
+    </div>
   )
 }
 
