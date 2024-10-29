b87cf2591bb93987b93b1efe25261a9c83e88997
Keren Villatoro <kerenv@outlook.com>
2024-10-28 19:30:25 -0700

    changes

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,25 +1,68 @@
+import React, { useEffect, useState } from 'react';
 import Accordion from "../components/accordion/Accordion"
-import { useState } from "react"
-import journalData from "../journal-data"
+import { useAuth0 } from "@auth0/auth0-react";
 
 const BlogPage = () => {
-  const [items, setItems] = useState(journalData)
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const { user } = useAuth0();
+
+  console.log(user);
+
+  if (!user) {
+      return null; // Optionally render a loading state
+  }
 
   const toggleAccordion = (id) => {
-    setItems(
-      items.map((item) =>
-        item.id === id
-          ? { ...item, show: !item.show }
-          : { ...item, show: false }
+    setPosts(
+      posts.map((posts) =>
+        posts.id === id
+          ? { ...posts, show: !posts.show }
+          : { ...posts, show: false }
       )
     )
   }
+  
+  useEffect(() => {
+    const fetchPosts = async () => {
+        try {
+            const response = await fetch('/api/posts/entries');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setPosts(data);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
+    fetchPosts();
+}, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
   return (
+    
     <>
-      <div className="text-4xl font-bold pb-4">
-        Blog Page
+       <div className="text-4xl font-bold pb-4">
+        My Developer Journey — So Far
       </div>
+      {posts.map((posts, i) => (
+        <Accordion
+          key={i}
+          id={posts.id}
+          toggleAccordion={toggleAccordion}
+          show={posts.show}
+          title={posts.title}
+        >
+          <p>{posts.content}</p>
+        </Accordion>
+      ))}
     </>
   )
 }
